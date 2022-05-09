9131dde3ab12f68ebf74d188bfa0dce526e1b81f
pauljwmiller <105019599+pauljwmiller@users.noreply.github.com>
2022-05-08 23:00:16 -0400

    Delete Buddhism Project directory

--- Buddhism Project/timer.js
+++
@@ -1,6 +0,0 @@
-var x = setInterval(function(){
-var timer = 600;
-var minutes = Math.floor(timer // 60);
-var seconds = Math.floor(timer % 60);
-index.getElementById("demo").innerHTML = minutes "minutes and " + seconds "seconds";
-}
